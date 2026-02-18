d4dcf4d4c89b1938a08522692b808ad47aa5c292
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2026-02-18 09:06:26 -0500

    added method to get new Id

--- Controller.cs
+++ Controller.cs
@@ -1,4 +1,8 @@
 
+using System;
+using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Model; 
 namespace Controller
 {
@@ -17,13 +21,46 @@ namespace Controller
         {
             TaskModel task = new TaskModel();   
             task.Description= Description;  
+            task.Status="todo"; 
+            task.CreationDate=DateOnly.FromDateTime(DateTime.Today); 
+            task.UpdateDate=task.CreationDate;  
+
             //pending : get the new Id to set  
 
             return -1;
         }
 
+        private string getTasksFile()
+        {
+            var file=Path.Join(Directory.GetCurrentDirectory(),"tasks.json");
+            return file;  
+        }
+        private int getNewId()
+        {
+            int Id=0; 
+            string file= this.getTasksFile(); 
+            string jsonString=File.ReadAllText(file);
+            if ((!File.Exists(file)) || (string.IsNullOrWhiteSpace(jsonString)) ){
+                Id=1;
 
 
+            }
+            else
+            {
+                var tasks=JsonSerializer.Deserialize<List<Task>>(jsonString);
+                if(tasks==null || tasks.Count == 0)
+                {
+                    Id=1;
+                }
+                else
+                {
+                    Id=tasks.Max(task =>task.Id)+1;  
+                }
+            }
+            
+
+            return Id; 
+        }
 
 
 
