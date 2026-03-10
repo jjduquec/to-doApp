d7caa580444a48bed9413c2b194cd6ee4766eef1
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2026-03-09 20:13:01 -0500

    created CommandView class

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using views;
+class ToDoApp
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            var graficalView= new GraficalView();  
+            graficalView.MainMenu();
+        }
+        else
+        {
+            var commandView= new CommandView();  
+            commandView.processArguments(args);
+        }
+    }
+
+
+    
+}
+
---
+++ views/command_view.cs
@@ -0,0 +1,17 @@
+using Controller; 
+class CommandView
+{
+    public CommandView(){}
+
+    public void processArguments(string[] parameters)
+    {
+        
+    }
+
+
+
+
+
+
+
+}
